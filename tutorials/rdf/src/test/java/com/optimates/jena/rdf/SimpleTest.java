77382544da4e914867703b301365383071eff796
jm2dev <jm@0pt1mates.com>
2015-09-15 23:08:45 +0100

    Add property to resource.# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## Date:      Tue Sep 15 23:08:45 2015 +0100## On branch master# Changes to be committed:#	modified:   rdf/build.gradle#	modified:   rdf/src/test/java/com/optimates/jena/rdf/SimpleTest.java#

--- tutorials/rdf/src/test/java/com/optimates/jena/rdf/SimpleTest.java
+++ tutorials/rdf/src/test/java/com/optimates/jena/rdf/SimpleTest.java
@@ -1,14 +1,40 @@
 package com.optimates.jena.rdf;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.VCARD;
 import org.junit.Test;
 
 public class SimpleTest {
     @Test
-    public void example() {
-        final String expected = "hola";
-        final String actual = "adios";
+    public void addPropertyToResource() throws Exception {
+        final String personUri = "http://somewhere/JohnSmith";
+        final String fullName = "John Smith";
 
-        assertThat(actual).isEqualTo(expected);
+        final Model model = ModelFactory.createDefaultModel();
+        final Resource johnSmith = model.createResource(personUri)
+                .addProperty(VCARD.FN, fullName);
+
+        assertThat(johnSmith.getURI()).isEqualTo(personUri);
+    }
+
+    @Test
+    public void addSeveralPropertiesWithCascadingStyle() throws Exception {
+        final String personUri = "http://somewhere/JohnSmith";
+        final String givenName = "John";
+        final String familyName = "Smith";
+        final String fullName = givenName + " " + familyName;
+        final Model model = ModelFactory.createDefaultModel();
+        final Resource johnSmith = model.createResource(personUri)
+                .addProperty(VCARD.FN, fullName)
+                .addProperty(VCARD.N,
+                        model.createResource()
+                                .addProperty(VCARD.Given, givenName)
+                                .addProperty(VCARD.Family, familyName));
+
+        assertThat(johnSmith.getURI()).isEqualTo(personUri);
     }
 }
